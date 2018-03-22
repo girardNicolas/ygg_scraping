4f5d05fdd76818d2e6741e8667ed9bde3fbe368d
Nicolas GIRARD <nicolas.girard00@gmail.com>
2018-03-22 09:35:22 +0100

    Add config parameter

--- settings/config.py
+++ settings/config.py
@@ -3,6 +3,8 @@ wait_time = 1
 results_number_by_page = 100
 log_file = 'ygg_scraping.log'
 database_path = 'ygg_database.db'
+max_fail_parsing_error = 10
+time_out = 5
 
 # REST API
 rest_api_debug = False
